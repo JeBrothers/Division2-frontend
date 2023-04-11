45683f267453eb7ea4a7131a53523d4e18adfcfb
donghyeon <wlalqjrj@gmail.com>
2023-04-12 05:30:54 +0900

    0412

--- pages/weapon/weaponinfo.tsx
+++ pages/weapon/weaponinfo.tsx
@@ -1,60 +1,125 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import { cls } from "../../libs/utils";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+interface WeaponInfo {
+  email?: string;
+  phone?: string;
+}
 
 const WeaponInfo: NextPage = () => {
+  const { register, handleSubmit, reset } = useForm<WeaponInfo>();
+  const [method, setMethod] = useState<
+    | "assultrifle"
+    | "rifle"
+    | "marksmanrifle"
+    | "submachinegun"
+    | "shotgun"
+    | "lightmachinegun"
+    | "pistol"
+  >("assultrifle");
+
+  const onArClick = () => {
+    reset();
+    setMethod("assultrifle");
+  };
+  const onRifleClick = () => {
+    reset();
+    setMethod("rifle");
+  };
+  const onMarksManRifleClick = () => {
+    reset();
+    setMethod("marksmanrifle");
+  };
+  const onSmgClick = () => {
+    reset();
+    setMethod("submachinegun");
+  };
+  const onShotgunClick = () => {
+    reset();
+    setMethod("shotgun");
+  };
+  const onLmgClick = () => {
+    reset();
+    setMethod("lightmachinegun");
+  };
+  const onPistolClick = () => {
+    reset();
+    setMethod("pistol");
+  };
   return (
     <>
       <Layout children />
 
       <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
         <ul
-          className="flex flex-wrap -mb-px text-sm font-medium text-center"
+          className="flex flex-wrap -mb-px text-sm font-medium text-center justify-center"
           id="myTab"
           data-tabs-toggle="#myTabContent"
           role="tablist"
         >
           <li className="mr-2" role="presentation">
             <button
-              className="inline-block p-4 border-b-2 rounded-t-lg"
+              id="AR"
+              data-tabs-target="#AR"
+              type="button"
+              role="tab"
+              aria-controls="AR"
+              aria-selected="true"
+              className={cls(
+                "inline-block p-4 border-b-2 rounded-t-lg",
+                method === "assultrifle"
+                  ? "border-division-orange text-division-orange"
+                  : "border-transparent   hover:text-gray-600 hover:border-gray-300 "
+              )}
+              onClick={onArClick}
+            >
+              돌격소총
+            </button>
+          </li>
+          <li className="mr-2" role="presentation">
+            <button
               id="profile-tab"
               data-tabs-target="#profile"
               type="button"
               role="tab"
               aria-controls="profile"
-              aria-selected="false"
+              aria-selected="true"
+              className={cls(
+                "inline-block p-4 border-b-2 rounded-t-lg",
+                method === "rifle"
+                  ? "border-division-orange text-division-orange"
+                  : "border-transparent   hover:text-gray-600 hover:border-gray-300 "
+              )}
+              onClick={onRifleClick}
             >
-              Profile
+              소총
             </button>
           </li>
           <li className="mr-2" role="presentation">
             <button
-              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              id="dashboard-tab"
-              data-tabs-target="#dashboard"
+              id="profile-tab"
+              data-tabs-target="#profile"
               type="button"
               role="tab"
-              aria-controls="dashboard"
-              aria-selected="false"
+              aria-controls="profile"
+              aria-selected="true"
+              className={cls(
+                "inline-block p-4 border-b-2 rounded-t-lg",
+                method === "marksmanrifle"
+                  ? "border-division-orange text-division-orange"
+                  : "border-transparent   hover:text-gray-600 hover:border-gray-300 "
+              )}
+              onClick={onMarksManRifleClick}
             >
-              Dashboard
-            </button>
-          </li>
-          <li className="mr-2" role="presentation">
-            <button
-              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              id="settings-tab"
-              data-tabs-target="#settings"
-              type="button"
-              role="tab"
-              aria-controls="settings"
-              aria-selected="false"
-            >
-              Settings
+              지정사수소총
             </button>
           </li>
           <li role="presentation">
             <button
-              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg active:border-division-orange hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
               id="contacts-tab"
               data-tabs-target="#contacts"
               type="button"
@@ -62,27 +127,145 @@ const WeaponInfo: NextPage = () => {
               aria-controls="contacts"
               aria-selected="false"
             >
-              Contacts
+              기관단총
+            </button>
+          </li>
+          <li role="presentation">
+            <button
+              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg active:border-division-orange hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              id="contacts-tab"
+              data-tabs-target="#contacts"
+              type="button"
+              role="tab"
+              aria-controls="contacts"
+              aria-selected="false"
+            >
+              샷건
+            </button>
+          </li>
+          <li role="presentation">
+            <button
+              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg active:border-division-orange hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              id="contacts-tab"
+              data-tabs-target="#contacts"
+              type="button"
+              role="tab"
+              aria-controls="contacts"
+              aria-selected="false"
+            >
+              권총
             </button>
           </li>
         </ul>
       </div>
+
       <div id="myTabContent">
-        <div
-          className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-          id="profile"
-          role="tabpanel"
-          aria-labelledby="profile-tab"
-        >
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            This is some placeholder content the{" "}
-            <strong className="font-medium text-gray-800 dark:text-white">
-              Profile tab's associated content
-            </strong>
-            . Clicking another tab will toggle the visibility of this one for
-            the next. The tab JavaScript swaps classes to control the content
-            visibility and styling.
-          </p>
+        <div id="AR" role="tabpanel" aria-labelledby="AR-tab">
+          <div className="flex relative overflow-x-auto justify-center">
+            <table className="w-max-md w-2/3 text-sm  text-gray-500 dark:text-gray-400 border-t border-gray-200 text-center ">
+              <thead className="text-xs text-white uppercase bg-division-dark dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-6">
+                    총기 종류
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    총기명
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    RPM
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    기본 탄창
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    최대 탄창
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    재장전 시간(빈 탄창)
+                  </th>
+                  <th scope="col" className="px-6 py-6">
+                    DPS
+                  </th>
+                  <th colSpan={4} scope="col" className=" justify-center ">
+                    부착물 슬롯
+                    <th className="w-1/4 px-12 py-4 bg-division-color text-division-dark whitespace-nowrap">
+                      조준기
+                    </th>
+                    <th
+                      scope="col"
+                      className="  w-1/4 px-12 bg-division-color text-division-dark whitespace-nowrap"
+                    >
+                      총구
+                    </th>
+                    <th
+                      scope="col"
+                      className=" w-1/4 px-12  bg-division-color text-division-dark whitespace-nowrap"
+                    >
+                      총열
+                    </th>
+                    <th
+                      scope="col"
+                      className=" w-1/4 px-12 bg-division-color text-division-dark whitespace-nowrap"
+                    >
+                      탄창
+                    </th>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    rowSpan={2}
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    ACR
+                  </th>
+                  <td className="px-6 py-4">ACR</td>
+                  <td className="px-6 py-4">650</td>
+                  <td className="px-6 py-4">30</td>
+                  <td className="px-6 py-4">50</td>
+                  <td className="px-6 py-4">2.3</td>
+                  <td className="px-6 py-4">575510</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="px-6 py-4">ACR-E</td>
+                  <td className="px-6 py-4">650</td>
+                  <td className="px-6 py-4">30</td>
+                  <td className="px-6 py-4">50</td>
+                  <td className="px-6 py-4">2.3</td>
+                  <td className="px-6 py-4">575510</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                </tr>
+                <tr className="bg-white dark:bg-gray-800">
+                  <th
+                    rowSpan={2}
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    AK-47
+                  </th>
+                  <td className="px-6 py-4">AK-M</td>
+                  <td className="px-6 py-4">600</td>
+                  <td className="px-6 py-4">30</td>
+                  <td className="px-6 py-4">50</td>
+                  <td className="px-6 py-4">2.5</td>
+                  <td className="px-6 py-4">620560</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">X</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                  <td className="px-6 py-4 text-center">O</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
         <div
           className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
