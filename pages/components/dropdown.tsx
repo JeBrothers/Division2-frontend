318b4b4eb827905c282f07d48472a37b73c4db7d
DJ <tkroahdla@naver.com>
2023-04-17 03:17:01 +0900

    drop down

--- pages/components/dropdown.tsx
+++ pages/components/dropdown.tsx
@@ -1,8 +1,12 @@
+import { useRouter } from "next/router";
+
 function Dropdown() {
+  const router = useRouter();
   return (
     <>
-      <li>마이페이지</li>
-      <li>로그아웃</li>
+      {/* onclick에 들어있는 화살표함수 console.log를 router.push로 변경하고 경로지정해주면 될듯 */}
+      <div className="cursor-pointer hover:bg-slate-800" onClick={() => { console.log("클릭") }}>마이페이지</div>
+      <div className="cursor-pointer hover:bg-slate-800" onClick={() => { console.log("클릭") }}>로그아웃</div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from "next";
+
+import { useState } from "react";
+import Dropdown from "./components/dropdown";
 import Layout from "./components/layout";
 
+
+
 const Home: NextPage = () => {
+  const [view, setView] = useState(false);
+
   return (
     <Layout>
-      <></>
+      <ul onClick={() => { setView(!view) }}>
+        반가워요, nickname 님!{" "}
+        {view ? '⌃' : '⌄'}
+        {view && <Dropdown />}
+      </ul>
     </Layout>
   );
 };
