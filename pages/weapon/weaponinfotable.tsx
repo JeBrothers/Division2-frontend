54ac52be820855ea3d8f81805bd5a7fc07910dad
donghyeon <wlalqjrj@gmail.com>
2023-05-03 15:32:11 +0900

    Revert "weaponTable"
    This reverts commit 467c927463c97d40b8020224d948e19e710fa6fa.

--- pages/weapon/weaponinfotable.tsx
+++ pages/weapon/weaponinfotable.tsx
@@ -2,64 +2,53 @@ import React from "react";
 import WeaponJson from "../../public/weapons.json";
 
 interface WeaponJsonDataDisplay {
-  ASSAULT_RIFLES: AR[],
-  LIGHT_MACHINE_GUNS: any[],
-  SUBMACHINE_GUNS: any[],
-  SHOTGUNS: any[],
-  RIFLES: any[],
-  MARKSMAN_RIFLES: any[],
-  PISTOLS: any[]
+  ASSALT_RIFLE: AR[];
 }
 
 interface AR {
   variant?: string;
   engName?: string;
-  name?: string;
-  baseMagSize?: number;
-  moddedMagSize?: number;
-  rpm?: number;
-  reload?: number;
-  damage?: number;
-  dps?: number;
-  muzzle?: boolean;
-  underbarrel?: boolean;
-  magazine?: boolean;
-  optics?: boolean;
-  dlc?: boolean;
-  exotic?: boolean;
-  named?: boolean;
+  name: string;
+  baseMagSize: number;
+  moddedMagSize: number;
+  rpm: number;
+  reload: number;
+  damage: number;
+  dps: number;
+  muzzle: boolean;
+  underbarrel: boolean;
+  magazine: boolean;
+  optics: boolean;
+  dlc: boolean;
+  exotic: boolean;
+  named: boolean;
   flavourText?: string;
 }
 
 export default function WeaponJsonDataDisplay() {
-  const DisplayData: WeaponJsonDataDisplay = WeaponJson
-
-  const data = DisplayData.ASSAULT_RIFLES!.map((info) => {
+  const DisplayData = WeaponJson.map((info: any) => {
     return (
       <tr>
-        <td>{info.name}</td>
-        <td>{info.engName}</td>
+        <td></td>
+        <td></td>
         <td></td>
       </tr>
     );
   });
-  // console.log(WeaponJson.ASSAULT_RIFLES[0].variant);
-  console.log(WeaponJson)
+  console.log(WeaponJson.ASSAULT_RIFLES[0].variant);
 
   return (
-    <>
-      <div>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>{data}</tbody>
-        </table>
-      </div>
-    </>
-  )
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{DisplayData}</tbody>
+      </table>
+    </div>
+  );
 }
