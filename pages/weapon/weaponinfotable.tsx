a14db76499276b5912350a4f2706cc99036133dd
donghyeon <wlalqjrj@gmail.com>
2023-05-05 20:49:36 +0900

    0505

--- pages/weapon/weaponinfotable.tsx
+++ pages/weapon/weaponinfotable.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import WeaponJson from "../../public/weapons.json";
-
-interface WeaponJsonDataDisplay {
-  ASSAULT_RIFLES: AR[],
-  LIGHT_MACHINE_GUNS: any[],
-  SUBMACHINE_GUNS: any[],
-  SHOTGUNS: any[],
-  RIFLES: any[],
-  MARKSMAN_RIFLES: any[],
-  PISTOLS: any[]
-}
+import AssualtRifle from "../../public/WeaponinfoJson/assaultrifle.json";
 
 interface AR {
   variant?: string;
@@ -32,34 +23,90 @@ interface AR {
 }
 
 export default function WeaponJsonDataDisplay() {
-  const DisplayData: WeaponJsonDataDisplay = WeaponJson
+  const DisplayData: AR = AssualtRifle;
 
-  const data = DisplayData.ASSAULT_RIFLES!.map((info) => {
+  const data = DisplayData.map((info) => {
     return (
-      <tr>
-        <td>{info.name}</td>
-        <td>{info.engName}</td>
-        <td></td>
+      <tr className="bg-white border-b border-x ">
+        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  border-x border-gray-200">
+          {info.variant}
+        </td>
+        <td className="px-6 py-4 border-x border-gray-200">{info.name}</td>
+        <td className="px-6 py-4 border-x border-gray-200">
+          {info.baseMagSize}
+        </td>
+        <td className="px-6 py-4 border-x border-gray-200">{info.rpm}</td>
+        <td className="px-6 py-4 border-x border-gray-200">{info.damage}</td>
+        <td className="px-6 py-4 border-x border-gray-200">{info.dps}</td>
+        <td className="px-6 py-4 text-center border-x border-gray-200">
+          {info.optics}
+        </td>
+        <td className="px-6 py-4 text-center border-x border-gray-200">
+          {info.muzzle}
+        </td>
+        <td className="px-6 py-4 text-center border-x border-gray-200">
+          {info.magazine}
+        </td>
+        <td className="px-6 py-4 text-center border-x border-gray-200">
+          {info.underbarrel}
+        </td>
       </tr>
     );
   });
-  // console.log(WeaponJson.ASSAULT_RIFLES[0].variant);
-  console.log(WeaponJson)
 
   return (
     <>
-      <div>
-        <table className="table table-striped">
-          <thead>
+      <div className="flex relative overflow-x-auto justify-center">
+        <table className="w-max-md w-2/3 text-sm  text-gray-500 dark:text-gray-400 border-t  border-gray-200 text-center ">
+          <thead className="text-xs text-white uppercase  bg-division-dark dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th></th>
-              <th></th>
-              <th></th>
+              <th scope="col" className="px-6  py-6">
+                총기종류
+              </th>
+              <th scope="col" className="px-6 py-6">
+                총기명
+              </th>
+              <th scope="col" className="px-6 py-6">
+                탄창용량
+              </th>
+              <th scope="col" className="px-6 py-6">
+                RPM
+              </th>
+              <th scope="col" className="px-6 py-6">
+                피해량
+              </th>
+              <th scope="col" className="px-6 py-6">
+                DPS
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-6 bg-division-color text-division-dark whitespace-nowrap"
+              >
+                조준기
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-6 bg-division-color text-division-dark whitespace-nowrap"
+              >
+                총구
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-6 bg-division-color text-division-dark whitespace-nowrap"
+              >
+                탄창
+              </th>
+              <td
+                scope="col"
+                className="px-6 py-6 bg-division-color text-division-dark whitespace-nowrap"
+              >
+                총열 하단부
+              </td>
             </tr>
           </thead>
           <tbody>{data}</tbody>
         </table>
       </div>
     </>
-  )
+  );
 }
