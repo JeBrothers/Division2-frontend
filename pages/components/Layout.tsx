93e03270c8a4131b8108053f552b5064c14581d8
donghyeon <wlalqjrj@gmail.com>
2023-05-23 03:51:57 +0900

    레이아웃 롤백

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -7,9 +7,6 @@ interface Layout {
 }
 
 export default function Layout() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
   return (
     <div className="relative mb-28">
       <div className="fixed top-0 left-0 right-0 z-10 ">
@@ -38,10 +35,7 @@ export default function Layout() {
 
                 <Menu as="div" className="relative inline-block text-left">
                   <div>
-                    <Menu.Button
-                      onClick={toggleMenu}
-                      className="${isMenuOpen ? 'active' : ''} flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-division-orange md:p-0 md:w-auto dark:text-white  dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-                    >
+                    <Menu.Button className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-division-orange md:p-0 md:w-auto dark:text-white  dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                       무기 정보
                       <svg
                         className="w-5 h-5 ml-1"
@@ -68,77 +62,75 @@ export default function Layout() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    {isMenuOpen && (
-                      <Menu.Items className="z-10 flex flex-col absolute -translate-x-12 mt-2 w-48  items-left origin-top-center divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="px-1 py-2 space-y-1 w-auto ">
-                          <Link href="/weapon/weaponinfo">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Menu.Button
-                                  className={`${
-                                    active
-                                      ? "bg-division-dark text-white"
-                                      : "text-gray-900"
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  <span>무기 정보</span>
-                                </Menu.Button>
-                              )}
-                            </Menu.Item>
-                          </Link>
+                    <Menu.Items className="z-10 flex flex-col absolute -translate-x-12 mt-2 w-48  items-left origin-top-center divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="px-1 py-2 space-y-1 w-auto ">
+                        <Link href="/weapon/weaponinfo">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Menu.Button
+                                className={`${
+                                  active
+                                    ? "bg-division-dark text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                <span>무기 정보</span>
+                              </Menu.Button>
+                            )}
+                          </Menu.Item>
+                        </Link>
 
-                          <Link href="/weapon/weaponaccessory">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Menu.Button
-                                  className={`${
-                                    active
-                                      ? "bg-division-dark text-white"
-                                      : "text-gray-900"
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  <span>무기 부착물</span>
-                                </Menu.Button>
-                              )}
-                            </Menu.Item>
-                          </Link>
+                        <Link href="/weapon/weaponaccessory">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Menu.Button
+                                className={`${
+                                  active
+                                    ? "bg-division-dark text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                <span>무기 부착물</span>
+                              </Menu.Button>
+                            )}
+                          </Menu.Item>
+                        </Link>
 
-                          <Link href="/weapon/weapontalent">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Menu.Button
-                                  className={`${
-                                    active
-                                      ? "bg-division-dark text-white"
-                                      : "text-gray-900"
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  무기 특수 효과
-                                </Menu.Button>
-                              )}
-                            </Menu.Item>
-                          </Link>
-                        </div>
+                        <Link href="/weapon/weapontalent">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Menu.Button
+                                className={`${
+                                  active
+                                    ? "bg-division-dark text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                무기 특수 효과
+                              </Menu.Button>
+                            )}
+                          </Menu.Item>
+                        </Link>
+                      </div>
 
-                        <div className="px-1 py-2 space-y-1 w-auto ">
-                          <Link href="/weapon/wnamedexotic">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <Menu.Button
-                                  className={`${
-                                    active
-                                      ? "bg-division-dark text-division-orange"
-                                      : "text-division-orange"
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  네임드 / 특급 아이템
-                                </Menu.Button>
-                              )}
-                            </Menu.Item>
-                          </Link>
-                        </div>
-                      </Menu.Items>
-                    )}
+                      <div className="px-1 py-2 space-y-1 w-auto ">
+                        <Link href="/weapon/wnamedexotic">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Menu.Button
+                                className={`${
+                                  active
+                                    ? "bg-division-dark text-division-orange"
+                                    : "text-division-orange"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                네임드 / 특급 아이템
+                              </Menu.Button>
+                            )}
+                          </Menu.Item>
+                        </Link>
+                      </div>
+                    </Menu.Items>
                   </Transition>
                 </Menu>
 
