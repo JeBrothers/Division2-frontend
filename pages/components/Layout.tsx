ce2361c872ed4de93c083390b139ebda9ba51cac
donghyeon <wlalqjrj@gmail.com>
2023-05-29 21:03:57 +0900

    0529 09:03

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,8 +1,6 @@
-import { Menu } from "@headlessui/react";
-
-import React, { useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
 import GearMenu from "./Menus/GearMenu";
-import SkillMenu from "./Menus/SkillMenu";
 import WeaponMenu from "./Menus/WeaponMenu";
 
 interface Layout {
@@ -50,7 +48,70 @@ export default function Layout() {
 
                 <GearMenu toggleMenu={toggleMenu} />
 
-                <SkillMenu toggleMenu={toggleMenu} />
+                <Menu
+                  as="div"
+                  className=" z-10 relative inline-block text-left"
+                >
+                  <div>
+                    <Menu.Button className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-division-orange md:p-0 md:w-auto dark:text-white  dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                      스킬
+                      <svg
+                        className="w-5 h-5 ml-1"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="flex flex-col absolute -translate-x-12 mt-2 w-48  items-left origin-top-center divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="px-1 py-2 space-y-1 w-auto ">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? "bg-division-dark text-white"
+                                  : "text-gray-900"
+                              } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                            >
+                              <span>스킬 리스트</span>
+                            </button>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? "bg-division-dark text-white"
+                                  : "text-gray-900"
+                              } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                            >
+                              <span>스킬 정보</span>
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
 
                 <Menu as="div" className="relative inline-block text-left">
                   <div>
